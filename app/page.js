aa05726e7226b1ddefc395e60ed660d1fbbc91cd
Md Rubel Hosen <rubelhosen1310@gmail.com>
2025-12-25 16:30:51 +0600

    css setup complete

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <a href='/about'>about</a>
+      <a href='/about' className='text-primary text-2xl'>
+        about
+      </a>
+
+      <p className='text-secondary'>jfldkas</p>
+
+      {/* Example for borders */}
+      <div className="border-primary p-4">
+        This div has a primary border color.
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
