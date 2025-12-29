1f685a8cd036f5324d528d44183ec9dee63cc36d
tauhid <tauhidbd789@gmail.com>
2025-12-29 12:29:48 +0600

    register page added

---
+++ app/(auth)/register/page.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/app/component/auth/RegisterForm";
+
+
+export const metadata = {
+  title: "iLMiFY | Register",
+};
+
+export default function RegisterPage() {
+  return <RegisterForm />; 
+}
