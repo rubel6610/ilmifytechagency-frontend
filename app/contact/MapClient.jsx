974f2529bfe2c95ac54b897a6b0ac528f5b65d40
hasibobydullah004 <hasibobydullah004@gmail.com>
2026-01-01 14:55:27 +0600

    fixed comments on components

--- app/contact/MapClient.jsx
+++ app/contact/MapClient.jsx
@@ -16,17 +16,3 @@ export default function MapClient() {
   );
 }
 
-//  Map section
-
-// "use client";
-
-// import dynamic from "next/dynamic";
-
-// const MapWithTooltip = dynamic(
-//   () => import("./MapWithTooltip"),
-//   { ssr: false }
-// );
-
-// export default function MapClient() {
-//   return <MapWithTooltip />;
-// }
