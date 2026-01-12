6a5bdcb9c167282de2d20089dd31bb7124f43ce2
sipon <fakhrulislamsipon@gmail.com>
2026-01-12 09:32:42 +0600

    reslove conflict

--- app/page.js
+++ app/page.js
@@ -15,72 +15,27 @@ import OverviewSection from "./component/OverviewSection";
 export default function Home() {
   return (
     <main className="overflow-hidden mt-25 md:mt-30 lg:mt-32">
-<<<<<<< HEAD
-      <Section>
-        <HeroSection />
-      </Section>
-
-      <Section>
-        <ServicesSection />
-      </Section>
-
-      <Section>
-        <WhoWeAre />
-      </Section>
-
-      <Section>
-        <ShowcaseSection />
-      </Section>
-
-      <Section>
-        <WeLoveWhatWeDo />
-      </Section>
-
-      <Section>
-        <Support />
-      </Section>
-
-      <Section>
-        <OverviewSection/>
-      </Section>
-
-      <Section>
-        <DigitalFutures />
-      </Section>
-
-      <Section>
-        <BlogPostSection />
-      </Section>
-
-      <Section>
-        <ContactCards />
-      </Section>
-
-      <Section>
-        <LogoSlider />
-      </Section>
-=======
-      {/* hero section */}
       <HeroSection />
-      {/* Service section */}
+
       <ServicesSection />
-      {/* who we are */}
+
       <WhoWeAre />
-      {/* Showcase section */}
+
       <ShowcaseSection />
-      {/* we love what we do */}
+
       <WeLoveWhatWeDo />
-      {/* Support */}
+
       <Support />
-      {/* Digital Futures */}
+
+      <OverviewSection />
+
       <DigitalFutures />
-      {/* BlogPostSection */}
+
       <BlogPostSection />
-      {/* Contact Cards */}
+
       <ContactCards />
-      {/* Logo Slider */}
+
       <LogoSlider />
->>>>>>> d58ffacb913c038a32d5279cc2222edaadc11e04
     </main>
   );
 }
